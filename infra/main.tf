60a4b49d767415d46afc1507ca6b2f674e71f6b4
TobiasLiu1990 <tobias.liu@live.se>
2023-11-15 21:56:42 +0100

    testing original main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,83 @@
+#resource "aws_apprunner_service" "service" {
+#  service_name = var.service_name
+#
+#  instance_configuration {
+#    cpu = "256"
+#    memory = "1024"
+#    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
+#  }
+#
+#  source_configuration {
+#    authentication_configuration {
+#      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
+#    }
+#    image_repository {
+#      image_configuration {
+#        port = var.port
+#      }
+#      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/ecr-kandidatnr-2038:latest"
+#      image_repository_type = "ECR"
+#    }
+#    auto_deployments_enabled = true
+#  }
+#}
+#
+#resource "aws_iam_role" "role_for_apprunner_service" {
+#  name               = var.aws_iam_role_name
+#  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+#}
+#
+#
+#data "aws_iam_policy_document" "assume_role" {
+#  statement {
+#    effect = "Allow"
+#
+#    principals {
+#      type        = "Service"
+#      identifiers = ["tasks.apprunner.amazonaws.com"]
+#    }
+#
+#    actions = ["sts:AssumeRole"]
+#  }
+#}
+#
+#data "aws_iam_policy_document" "policy" {
+#  statement {
+#    effect    = "Allow"
+#    actions   = ["rekognition:*"]
+#    resources = ["*"]
+#  }
+#
+#  statement  {
+#    effect    = "Allow"
+#    actions   = ["s3:*"]
+#    resources = ["*"]
+#  }
+#
+#  statement  {
+#    effect    = "Allow"
+#    actions   = ["cloudwatch:*"]
+#    resources = ["*"]
+#  }
+#}
+#
+#resource "aws_iam_policy" "policy" {
+#  name        = var.aws_iam_policy_name
+#  description = "Policy for App Runner"
+#  policy      = data.aws_iam_policy_document.policy.json
+#}
+#
+#
+#resource "aws_iam_role_policy_attachment" "attachment" {
+#  role       = aws_iam_role.role_for_apprunner_service.name
+#  policy_arn = aws_iam_policy.policy.arn
+#}
+#
+
 resource "aws_apprunner_service" "service" {
-  service_name = var.service_name
+  service_name = "kjell-is-king"
 
   instance_configuration {
-    cpu = "256"
-    memory = "1024"
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
   }
 
@@ -13,9 +87,9 @@ resource "aws_apprunner_service" "service" {
     }
     image_repository {
       image_configuration {
-        port = var.port
+        port = "8080"
       }
-      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/ecr-kandidatnr-2038:latest"
+      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/kjell:latest"
       image_repository_type = "ECR"
     }
     auto_deployments_enabled = true
@@ -23,7 +97,7 @@ resource "aws_apprunner_service" "service" {
 }
 
 resource "aws_iam_role" "role_for_apprunner_service" {
-  name               = var.aws_iam_role_name
+  name               = "kjell-role-thingy"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -47,7 +121,7 @@ data "aws_iam_policy_document" "policy" {
     actions   = ["rekognition:*"]
     resources = ["*"]
   }
-  
+
   statement  {
     effect    = "Allow"
     actions   = ["s3:*"]
@@ -62,8 +136,8 @@ data "aws_iam_policy_document" "policy" {
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = var.aws_iam_policy_name
-  description = "Policy for App Runner"
+  name        = "kjell-apr-policy-thingy"
+  description = "Policy for apprunner instance I think"
   policy      = data.aws_iam_policy_document.policy.json
 }
 
@@ -72,4 +146,3 @@ resource "aws_iam_role_policy_attachment" "attachment" {
   role       = aws_iam_role.role_for_apprunner_service.name
   policy_arn = aws_iam_policy.policy.arn
 }
-
