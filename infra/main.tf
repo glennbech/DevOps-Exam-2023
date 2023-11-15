4346f6ccd2ff6795109d9b4bd00e38b13ef256e1
TobiasLiu1990 <tobias.liu@live.se>
2023-11-15 20:54:01 +0100

    Modifying main.tf and adding cpu/memory usage

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,15 @@
 resource "aws_apprunner_service" "service" {
-  service_name = string
-  default: "apprunner-2038"
+  service_name = var.service_name
+
+  # Set CPU
+  instance_configuration {
+    cpu = "256"
+  }
+
+  # Set memory
+  instance_configuration {
+    memory = "1024"
+  }
 
   instance_configuration {
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
@@ -12,7 +21,8 @@ resource "aws_apprunner_service" "service" {
     }
     image_repository {
       image_configuration {
-        port = "8080"
+        port = int
+        default = "8080"
       }
       image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/kjell:latest"
       image_repository_type = "ECR"
@@ -22,7 +32,7 @@ resource "aws_apprunner_service" "service" {
 }
 
 resource "aws_iam_role" "role_for_apprunner_service" {
-  name               = "kjell-role-thingy"
+  name               = var.aws_iam_role_name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -61,8 +71,8 @@ data "aws_iam_policy_document" "policy" {
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "kjell-apr-policy-thingy"
-  description = "Policy for apprunner instance I think"
+  name        = var.aws_iam_policy_name
+  description = "Policy for App Runner"
   policy      = data.aws_iam_policy_document.policy.json
 }
 
---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "service_name" {
+  default = "app-service-2038"
+  type = string
+}
+
+variable "aws_iam_role_name" {
+  default = "iam-role-2038"
+  type = string
+}
+
+variable "aws_iam_policy_name" {
+  default = "policy-app-service-2038"
+  type = string
+}
