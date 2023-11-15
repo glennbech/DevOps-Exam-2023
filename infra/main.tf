116e5504a1072173c0ea613183a56f62cd010042
TobiasLiu1990 <tobias.liu@live.se>
2023-11-15 22:09:56 +0100

    Test again

--- infra/main.tf
+++ infra/main.tf
@@ -1,83 +1,9 @@
-#resource "aws_apprunner_service" "service" {
-#  service_name = var.service_name
-#
-#  instance_configuration {
-#    cpu = "256"
-#    memory = "1024"
-#    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
-#  }
-#
-#  source_configuration {
-#    authentication_configuration {
-#      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
-#    }
-#    image_repository {
-#      image_configuration {
-#        port = var.port
-#      }
-#      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/ecr-kandidatnr-2038:latest"
-#      image_repository_type = "ECR"
-#    }
-#    auto_deployments_enabled = true
-#  }
-#}
-#
-#resource "aws_iam_role" "role_for_apprunner_service" {
-#  name               = var.aws_iam_role_name
-#  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-#}
-#
-#
-#data "aws_iam_policy_document" "assume_role" {
-#  statement {
-#    effect = "Allow"
-#
-#    principals {
-#      type        = "Service"
-#      identifiers = ["tasks.apprunner.amazonaws.com"]
-#    }
-#
-#    actions = ["sts:AssumeRole"]
-#  }
-#}
-#
-#data "aws_iam_policy_document" "policy" {
-#  statement {
-#    effect    = "Allow"
-#    actions   = ["rekognition:*"]
-#    resources = ["*"]
-#  }
-#
-#  statement  {
-#    effect    = "Allow"
-#    actions   = ["s3:*"]
-#    resources = ["*"]
-#  }
-#
-#  statement  {
-#    effect    = "Allow"
-#    actions   = ["cloudwatch:*"]
-#    resources = ["*"]
-#  }
-#}
-#
-#resource "aws_iam_policy" "policy" {
-#  name        = var.aws_iam_policy_name
-#  description = "Policy for App Runner"
-#  policy      = data.aws_iam_policy_document.policy.json
-#}
-#
-#
-#resource "aws_iam_role_policy_attachment" "attachment" {
-#  role       = aws_iam_role.role_for_apprunner_service.name
-#  policy_arn = aws_iam_policy.policy.arn
-#}
-#
-
 resource "aws_apprunner_service" "service" {
-  service_name = "kjell-is-king"
+  service_name = var.service_name
 
   instance_configuration {
+    cpu = "256"
+    memory = "1024"
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
   }
 
@@ -87,9 +13,9 @@ resource "aws_apprunner_service" "service" {
     }
     image_repository {
       image_configuration {
-        port = "8080"
+        port = var.port
       }
-      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/kjell:latest"
+      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/ecr-kandidatnr-2038:latest"
       image_repository_type = "ECR"
     }
     auto_deployments_enabled = true
@@ -97,7 +23,7 @@ resource "aws_apprunner_service" "service" {
 }
 
 resource "aws_iam_role" "role_for_apprunner_service" {
-  name               = "kjell-role-thingy"
+  name               = var.aws_iam_role_name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -136,8 +62,8 @@ data "aws_iam_policy_document" "policy" {
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "kjell-apr-policy-thingy"
-  description = "Policy for apprunner instance I think"
+  name        = var.aws_iam_policy_name
+  description = "Policy for App Runner"
   policy      = data.aws_iam_policy_document.policy.json
 }
 
@@ -146,3 +72,4 @@ resource "aws_iam_role_policy_attachment" "attachment" {
   role       = aws_iam_role.role_for_apprunner_service.name
   policy_arn = aws_iam_policy.policy.arn
 }
+
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,19 @@
-#variable "service_name" {
-#  default = "app-service-2038"
-#  type = string
-#}
-#
-#variable "aws_iam_role_name" {
-#  default = "iam-role-2038"
-#  type = string
-#}
-#
-#variable "aws_iam_policy_name" {
-#  default = "policy-app-service-2038"
-#  type = string
-#}
-#
-#variable "port" {
-#  default = "8080"
-#  type = string
-#}
+variable "service_name" {
+  default = "app-service-2038"
+  type = string
+}
+
+variable "aws_iam_role_name" {
+  default = "iam-role-2038"
+  type = string
+}
+
+variable "aws_iam_policy_name" {
+  default = "policy-app-service-2038"
+  type = string
+}
+
+variable "port" {
+  default = "8080"
+  type = string
+}
