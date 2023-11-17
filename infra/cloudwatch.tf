631a89bfb68b7c3033f85cf843e87e42a2072892
TobiasLiu1990 <tobias.liu@live.se>
2023-11-17 20:37:45 +0100

    Updated cloudwatch.tf to show violation alarms as gauge

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -21,20 +21,26 @@ resource "aws_cloudwatch_dashboard" "main" {
       }
     },
     {
-      "type": "bar",
-      "x": 0,
-      "y": 6,
-      "width": 12,
-      "height": 6,
-      "properties": {
+     "type": "metric",
+     "x": 18,
+     "y"; 60,
+     "width": 6,
+     "height": 6,
+     "properties": {
         "metrics": [
-          [ "${var.cloudwatch_namespace}", "total_violations.value"],
-          [ "${var.cloudwatch_namespace}", "total_valid.value"]
+          ["${var.cloudwatch_namespace}, "exceeded_violation_alarm"]
         ],
+        "view": "gauge",
+        "title": "Violation alarm counter",
         "region": "eu-west-1",
-        "title": "PPE Detection analysis - Bar"
-      }
-    }
+        "yAxis": {
+           "left": {
+              "min": 0,
+              "max": 100,
+           }
+         }
+       }
+     }
   ]
 }
 DASHBOARD
