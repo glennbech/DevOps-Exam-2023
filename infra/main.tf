ed9b3c737e6257d308349db4c3617ebf424e3644
TobiasLiu1990 <tobias.liu@live.se>
2023-11-18 17:07:24 +0100

    Forgot to uncomment main.tf. Also pushing a application.properties file this time, but it does not contain sensitive data

--- infra/main.tf
+++ infra/main.tf
@@ -1,76 +1,76 @@
-#resource "aws_apprunner_service" "service" {
-#  service_name = var.service_name
-#
-#  instance_configuration {
-#    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
-#    cpu = "256"
-#    memory = "1024"
-#  }
-#
-#  source_configuration {
-#    authentication_configuration {
-#      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
-#    }
-#    image_repository {
-#      image_configuration {
-#        port = var.port
-#      }
-##      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/ecr-kandidatnr-2038:latest"
-#      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/ecr-kandidatnr-2038:${var.image_tag}"
-#      image_repository_type = "ECR"
-#    }
-#    auto_deployments_enabled = true
-#  }
-#}
-#
-#resource "aws_iam_role" "role_for_apprunner_service" {
-#  name               = var.aws_iam_role_name
-#  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-#}
-#
-#
-#data "aws_iam_policy_document" "assume_role" {
-#  statement {
-#    effect = "Allow"
-#
-#    principals {
-#      type        = "Service"
-#      identifiers = ["tasks.apprunner.amazonaws.com"]
-#    }
-#
-#    actions = ["sts:AssumeRole"]
-#  }
-#}
-#
-#data "aws_iam_policy_document" "policy" {
-#  statement {
-#    effect    = "Allow"
-#    actions   = ["rekognition:*"]
-#    resources = ["*"]
-#  }
-#
-#  statement  {
-#    effect    = "Allow"
-#    actions   = ["s3:*"]
-#    resources = ["*"]
-#  }
-#
-#  statement  {
-#    effect    = "Allow"
-#    actions   = ["cloudwatch:*"]
-#    resources = ["*"]
-#  }
-#}
-#
-#resource "aws_iam_policy" "policy" {
-#  name        = var.aws_iam_policy_name
-#  description = "Policy for App Runner"
-#  policy      = data.aws_iam_policy_document.policy.json
-#}
-#
-#
-#resource "aws_iam_role_policy_attachment" "attachment" {
-#  role       = aws_iam_role.role_for_apprunner_service.name
-#  policy_arn = aws_iam_policy.policy.arn
-#}
-#
+resource "aws_apprunner_service" "service" {
+  service_name = var.service_name
+
+  instance_configuration {
+    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
+    cpu = "256"
+    memory = "1024"
+  }
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
+    }
+    image_repository {
+      image_configuration {
+        port = var.port
+      }
+#      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/ecr-kandidatnr-2038:latest"
+      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/ecr-kandidatnr-2038:${var.image_tag}"
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = true
+  }
+}
+
+resource "aws_iam_role" "role_for_apprunner_service" {
+  name               = var.aws_iam_role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["tasks.apprunner.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["rekognition:*"]
+    resources = ["*"]
+  }
+
+  statement  {
+    effect    = "Allow"
+    actions   = ["s3:*"]
+    resources = ["*"]
+  }
+
+  statement  {
+    effect    = "Allow"
+    actions   = ["cloudwatch:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = var.aws_iam_policy_name
+  description = "Policy for App Runner"
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "attachment" {
+  role       = aws_iam_role.role_for_apprunner_service.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
