5cff703f986879ee788280073709534e778b2f08
TobiasLiu1990 <tobias.liu@live.se>
2023-11-18 17:03:12 +0100

    Updating workflows

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,32 +1,31 @@
 variable "service_name" {
-  default = "app-runner-kandidatnr-2038"
+#  default = "app-runner-kandidatnr-2038"
   type = string
 }
 
 variable "aws_iam_role_name" {
-  default = "policy-app-service-2038"
+#  default = "policy-app-service-2038"
   type = string
 }
 
 variable "aws_iam_policy_name" {
-  default = "iam-role-2038"
+#  default = "iam-role-2038"
   type = string
 }
 
 variable "port" {
-  default = "8080"
+#  default = "8080"
   type = string
 }
 
 # default is set from github actions during docker push to ecr.
 variable "image_tag" {
-  default = "blabla-tempremovelater"
   type = string
 }
 
 # Used for both namespace for CloudWatch dashboard and alarm.
 variable "prefix" {
-  default = "cloudwatch-kandidatnr-2038"
+#  default = "cloudwatch-kandidatnr-2038"
   type = string
 }
 
