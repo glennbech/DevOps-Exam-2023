284cacd299a2b0d764eff3af146043634b7f8f00
TobiasLiu1990 <tobias.liu@live.se>
2023-11-18 15:29:44 +0100

    Fixed namespace, forgot to remove "" around variable name..

--- src/main/java/com/example/s3rekognition/config/MetricsConfig.java
+++ src/main/java/com/example/s3rekognition/config/MetricsConfig.java
@@ -42,7 +42,7 @@ public class MetricsConfig {
     private CloudWatchConfig setupCloudWatchConfig() {
         return new CloudWatchConfig() {
             private Map<String, String> configuration = Map.of(
-                    "cloudwatch.namespace", "cloudwatchNamespace",
+                    "cloudwatch.namespace", cloudwatchNamespace,
                     "cloudwatch.step", Duration.ofSeconds(5).toString());
             @Override
             public String get(String key) {
